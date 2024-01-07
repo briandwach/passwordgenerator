d524f1203f33bdd5f27c0cff984ef20c1c98a30e
livefromthelevel <brian.d.wach@gmail.com>
2024-01-06 21:51:58 -0600

    <Added javascript logic for prompts to confirm characters in password generator>

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ var generateBtn = document.querySelector("#generate");
 var password = "Your Secure Password";
 var passLength = null;
 var passwordHTML = document.querySelector("#password");
+var passCharLower;
+var passCharUpper;
+var passCharNumeric;
+var passCharSpecial;
 
 
 //Functions for password generation declared:
@@ -17,7 +21,7 @@ function writePassword() {
 //Syntax Errors
 // ------------------------------------------------------------
 
-//Any insufficient criteria causing password generation to need to be restarted
+//Any insufficient criteria requiring password generation to start over
 function insufficientCriteria() {
   password = "";
   passwordHTML.placeholder = "Insufficient Criteria Chosen:\nPlease Generate Password Again";
@@ -25,9 +29,119 @@ function insufficientCriteria() {
 };
 
 
-// Password length syntax error
+//Password length syntax error
 function funLengthSyntax() {
-  passLength = window.prompt("Syntax Error:  Please enter password length as an integer (8-128)");
+  passLength = window.prompt("Syntax Error:\nPlease choose password length as an integer (Enter # 8-128)");
+
+  if (passLength === null) {
+    insufficientCriteria();
+    return;
+  };
+
+  passLength = Number(passLength);
+
+  if (!Number.isInteger(passLength)
+    || (passLength < 8)
+    || (passLength > 128)) {
+    funLengthSyntax();
+  } else {
+    return;
+  };
+};
+
+//Inlcude lowercase characters syntax error
+function funPassLowercaseSyntax() {
+  passCharLower = window.prompt("Current Password Critera\nLength: " + passLength +
+    "\n\nSyntax Error:\nPlease confirm include lowercase characters? (Enter Y or N)");
+
+  if (passCharLower === null) {
+    insufficientCriteria();
+    return;
+  };
+
+  passCharLower = passCharLower.toUpperCase();
+
+  if (passCharLower != "Y" && passCharLower != "N") {
+    funPassLowercaseSyntax();
+  } else {
+    return;
+  };
+};
+
+
+//Include uppercase characters syntax error
+function funPassUppercaseSyntax() {
+  passCharUpper = window.prompt("Current Password Criteria\nLength: " + passLength +
+    "\nLowercase characters: " + passCharLower +
+    "\n\nSyntax Error:\nInclude uppercase characters? (Enter Y or N)");
+
+  if (passCharUpper === null) {
+    insufficientCriteria();
+    return;
+  };
+
+  passCharUpper = passCharUpper.toUpperCase();
+
+  if (passCharUpper != "Y" && passCharUpper != "N") {
+    funPassUppercaseSyntax();
+  } else {
+    return;
+  };
+};
+
+//Inlcude numeric characters syntax error
+function funPassNumericSyntax() {
+  passCharNumeric = window.prompt("Current Password Criteria\nLength: " + passLength +
+    "\nLowercase characters: " + passCharLower +
+    "\nUppercase characters: " + passCharUpper +
+    "\n\nSyntax Error:\nInclude numeric characters? (Enter Y or N)");
+
+  if (passCharNumeric === null) {
+    insufficientCriteria();
+    return;
+  };
+
+  passCharNumeric = passCharNumeric.toUpperCase();
+
+  if (passCharNumeric != "Y" && passCharNumeric != "N") {
+    funPassNumericSyntax();
+  } else {
+    return;
+  };
+};
+
+//Include special characters syntax error
+function funPassSpecialSyntax() {
+  passCharSpecial = window.prompt("Current Password Criteria\nLength: " + passLength +
+    "\nLowercase characters: " + passCharLower +
+    "\nUppercase characters: " + passCharUpper +
+    "\nNumeric characters: " + passCharNumeric +
+    "\n\nSyntax Error:\nInclude special characters? (Enter Y or N)");
+
+  if (passCharSpecial === null) {
+    insufficientCriteria();
+    return;
+  };
+
+  passCharSpecial = passCharSpecial.toUpperCase();
+
+  if (passCharSpecial != "Y" && passCharSpecial != "N") {
+    funPassSpecialSyntax();
+  } else {
+    return;
+  };
+};
+
+
+
+
+//Clean syntax from user input
+// ------------------------------------------------------------
+
+
+// Password length prompt function
+function funPassLength() {
+  passLength = window.prompt("Choose password length (Enter # 8-128)");
 
   if (passLength === null) {
     insufficientCriteria();
@@ -46,27 +160,83 @@ function funLengthSyntax() {
 };
 
 
+//Confirm use of lowercase charaters function
+function funPassLowercase() {
+  passCharLower = window.prompt("Current Password Criteria\nLength: " + passLength +
+    "\n\nInclude lowercase characters? (Enter Y or N)");
 
-
-//Clean syntax from user input
-// ------------------------------------------------------------
-
-
-// Password length variable declared and generated
-function funPassLength() {
-  passLength = window.prompt("Choose password length (8-128)");
-
-  if (passLength === null) {
+  if (passCharLower === null) {
     insufficientCriteria();
     return;
   };
 
-  passLength = Number(passLength);
+  passCharLower = passCharLower.toUpperCase();
 
-  if (!Number.isInteger(passLength)
-    || (passLength < 8)
-    || (passLength > 128)) {
-    funLengthSyntax();
+  if (passCharLower !== "Y" && passCharLower !== "N") {
+    funPassLowercaseSyntax();
+  } else {
+    return;
+  };
+};
+
+//Confirm use of uppcase charaters function
+function funPassUppercase() {
+  passCharUpper = window.prompt("Current Password Criteria\nLength: " + passLength +
+    "\nLowercase characters: " + passCharLower +
+    "\n\nInclude uppercase characters? (Enter Y or N)");
+
+  if (passCharUpper === null) {
+    insufficientCriteria();
+    return;
+  };
+
+  passCharUpper = passCharUpper.toUpperCase();
+
+  if (passCharUpper != "Y" && passCharUpper != "N") {
+    funPassUppercaseSyntax();
+  } else {
+    return;
+  };
+};
+
+//Confirm use of numeric characters function
+function funPassNumeric() {
+  passCharNumeric = window.prompt("Current Password Criteria\nLength: " + passLength +
+    "\nLowercase characters: " + passCharLower +
+    "\nUppercase characters: " + passCharUpper +
+    "\n\nInclude numeric characters? (Enter Y or N)");
+
+  if (passCharNumeric === null) {
+    insufficientCriteria();
+    return;
+  };
+
+  passCharNumeric = passCharNumeric.toUpperCase();
+
+  if (passCharNumeric != "Y" && passCharNumeric != "N") {
+    funPassNumericSyntax();
+  } else {
+    return;
+  };
+};
+
+//Confirm use of special characters function
+function funPassSpecial() {
+  passCharSpecial = window.prompt("Current Password Criteria\nLength: " + passLength +
+    "\nLowercase characters: " + passCharLower +
+    "\nUppercase characters: " + passCharUpper +
+    "\nNumeric characters: " + passCharNumeric +
+    "\n\nInclude special characters? (Enter Y or N)");
+
+  if (passCharSpecial === null) {
+    insufficientCriteria();
+    return;
+  };
+
+  passCharSpecial = passCharSpecial.toUpperCase();
+
+  if (passCharSpecial != "Y" && passCharSpecial != "N") {
+    funPassSpecialSyntax();
   } else {
     return;
   };
@@ -80,11 +250,56 @@ function generatePassword() {
   //Begins by calling functions for prompts which also check syntax
   funPassLength();
 
+  if (passLength !== null) {
+    funPassLowercase();
+  } else {
+    passwordHTML.value = "";
+    passwordHTML.placeholder = "Insufficient Criteria Chosen:\nPlease Generate Password Again";
+    return;
+  }
+
+  //Omitting this conversion to uppercase for now.  Instead converting variable to yes or no.
+  //passCharLower = passCharLower.toUpperCase();
+
+  if (passCharLower !== null) {
+    if (passCharLower == "Y") {
+      passCharLower = "Yes";
+    } else { passCharLower = "No" };
+    funPassUppercase();
+  } else {
+    passwordHTML.value = "";
+    passwordHTML.placeholder = "Insufficient Criteria Chosen:\nPlease Generate Password Again";
+    return;
+  };
+
+  if (passCharUpper !== null) {
+    if (passCharUpper == "Y") {
+      passCharUpper = "Yes";
+    } else { passCharUpper = "No" };
+    funPassNumeric();
+  } else {
+    passwordHTML.value = "";
+    passwordHTML.placeholder = "Insufficient Criteria Chosen:\nPlease Generate Password Again";
+    return;
+  };
+
+  if (passCharNumeric !== null) {
+    if (passCharNumeric == "Y") {
+      passCharNumeric = "Yes";
+    } else { passCharNumeric = "No" };
+    funPassSpecial();
+  } else {
+    passwordHTML.value = "";
+    passwordHTML.placeholder = "Insufficient Criteria Chosen:\nPlease Generate Password Again";
+    return;
+  };
+
+
   //Once functions for criteria are working, a password will then need to be generated following the criteria
 
   //Final password written here if applicable.
-  if (passLength !== null) {
-    password = String(passLength);
+  if (passLength !== null && passCharLower !== null && passCharUpper !== null && passCharNumeric !== null && passCharSpecial !== null) {
+    password = (String(passLength) + passCharLower + passCharUpper + passCharNumeric + passCharSpecial);
     writePassword();
   } else {
     passwordHTML.value = "";
@@ -96,16 +311,18 @@ function generatePassword() {
 
 
 // First writes password to the #password input (I have changed password to be a global variable)
-function startPassword() {
+function startNewPassword() {
   passwordHTML.value = "";
   passwordHTML.placeholder = "Follow Criteria Prompts";
+
+  //setTimout is necessary for the above placeholder text change to be visible before window.prompt shows
   setTimeout(generatePassword, 100);
 };
 
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", startPassword);
+generateBtn.addEventListener("click", startNewPassword);
 
 
 
